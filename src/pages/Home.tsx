56971d1b02b69a994b63c76ff9fff8ffccf4df39
rusmarch <cheustor@gmail.com>
2024-03-21 15:29:37 +0200

    auth3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -46,6 +46,7 @@ export const Home = () => {
       if (isAuth || user) {
          navigate('/')
       }
+      console.log('before reset');
       dispatch(reset());
    }, [isError, isAuth, user, message, navigate, dispatch])
 
